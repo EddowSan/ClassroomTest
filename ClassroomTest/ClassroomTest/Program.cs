e1b9f65587b6fbf27706a84c4a84e978f048f2d7
derek <DerekEddow@gmail.com>
2012-02-13 07:59:10 -0800

    added code

---
+++ ClassroomTest/ClassroomTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a man from Nantucket...");
+        }
+    }
+}
